4559c1b312feb3789c72fdc41fe03cdde974b7a3
sharmisthachoudhuri <sharmistha.choudhuri193@gmail.com>
2022-04-09 13:09:02 +0530

    Spark streaming fix

--- src/main/scala/sparkstreaming/basics/ReadFromTopic.scala
+++ src/main/scala/sparkstreaming/basics/ReadFromTopic.scala
@@ -7,13 +7,14 @@ object ReadFromTopic {
     .appName("Integrating Kafka")
     .master("local[2]")
     .getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
 
   def readFromKafka() = {
     // Subscribe to 1 topic
     val kafkaDF: DataFrame = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "127.0.0.1:9092")
-      .option("subscribe", "employee")
+      .option("subscribe", "test")
       .option("startingOffsets", "earliest") // From starting
       .load()
 
@@ -25,17 +26,6 @@ object ReadFromTopic {
       .outputMode("append")
       .start()
       .awaitTermination()
-
-    /*
-    kafkaDF
-      .select(col("topic"), expr("cast(value as string) as actualValue"))
-      .writeStream
-      .format("console")
-      .outputMode("append")
-      .start()
-      .awaitTermination()
-
-     */
   }
 
   def main(args: Array[String]): Unit = {
