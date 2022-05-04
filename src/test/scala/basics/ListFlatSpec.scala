241b62614eec3ed9b5b6c19469b97d6e39e2faaa
sharmisthachoudhuri <sharmistha.choudhuri193@gmail.com>
2022-05-04 22:29:47 +0530

    Added scala and spark testing libraries

---
+++ src/test/scala/basics/ListFlatSpec.scala
@@ -0,0 +1,18 @@
+package basics
+
+import org.scalatest.FlatSpec
+
+class ListFlatSpec extends FlatSpec {
+
+  "An empty List" should "have size 0" in {
+    assert(List.empty.size == 0)
+  }
+
+  it should "throw an IndexOutOfBoundsException when trying to access any element" in {
+    val emptyList = List();
+    assertThrows[IndexOutOfBoundsException] {
+      emptyList(1)
+    }
+  }
+
+}
